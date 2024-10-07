db0dfaf55ac635ce6157499c3201e0171f56d454
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-10-07 19:03:30 +0900

    코드 분석

--- DFS.java
+++ DFS.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class DFS {
+    //IOException : 자바에서 입출력 작업 중 발생하는 예외를 처리하는 것
     public static void main(String[] args) throws IOException {
         //데이터를 버퍼에 정하면서 읽기 때문에, 많은 양의 데이터를 다룰 때 속도가 빠르다.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -8,9 +9,13 @@ public class DFS {
         StringBuilder sb = new StringBuilder();
         //연비 입력
         String[] NQ = br.readLine().split(" ");
+        //NQ에다가 저장 N이 연비 데이터의 개수
         int N = Integer.parseInt(NQ[0]);
+        //정수로 변환하여 변수 N에 저장
         int Q = Integer.parseInt(NQ[1]);
+        //Q는 연비 쿼리의 개수를 의미
         int[] arr = new int[N];
+        //N은 연비 데이터 개수, Q는 연비 쿼리의 수
 
         //배열 정렬 
         String[] line = br.readLine().split(" ");
