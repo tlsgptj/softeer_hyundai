67bdf2f2dcbdde879e513ed98a7f51e5f97707ed
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-10-08 13:40:19 +0900

    BFS 하는중

--- BFS.java
+++ BFS.java
@@ -1,3 +1,11 @@
+import java.util.*;
+import java.io.*;
+
 public class BFS {
+    // 이동할 네 가지 방향 (상, 하, 좌, 우)
+    static int[] dx = {-1, 1, 0, 0}
+    static int[] dy = {0, 0, -1, 1}
+
+    //미로의 크기
     
 }
